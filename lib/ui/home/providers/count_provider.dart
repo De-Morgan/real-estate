df1bd89b32b701fab472b44ceb869e6aac076a75
Michael Adeyemo <michaeladesola1410@gmail.com>
2024-06-21 09:59:48 +0100

    removed commented out code

--- lib/ui/home/providers/count_provider.dart
+++ lib/ui/home/providers/count_provider.dart
@@ -2,14 +2,6 @@ import 'dart:async';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// final countProvider =
-//     StreamProvider.autoDispose.family<int, int>((ref, seed) async* {
-//   final timer = Timer.periodic(const Duration(milliseconds: 200), (timer) {
-//      4;
-//   });
-//
-// });
-
 class CountNotifier extends FamilyNotifier<int, int> {
   @override
   int build(int arg) {
